a5b8e9afb45d898a32163b8b6841187deb02a459
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-04-07 03:00:44 -0300

    changed typeWrite method

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-const nome = document.querySelector('h1');
+const nome = document.querySelector('cite');
 
 // animação do nome...
 const typeWrite = (elemento) => {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 120 * i);
+        setTimeout(() => elemento.innerHTML += letra, 260 * i);
     });
 }
 
