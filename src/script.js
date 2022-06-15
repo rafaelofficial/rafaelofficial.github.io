16c810c9359d202f559667c3c5c466b08294b223
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-06-14 21:50:51 -0300

    feat: added hamburger menu

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ const typeWrite = (elemento) => {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 260 * i);
+        setTimeout(() => elemento.innerHTML += letra, 160 * i);
     });
 }
 
